2b4e2e30b7d5cd39cd397bda76e6bdfa6b446af1
Alanis Oliveira <84356073+AlanisOliveira@users.noreply.github.com>
2023-11-14 17:24:45 -0300

    Update tabelas - restaurante.sql

--- tabelas - restaurante.sql
+++ tabelas - restaurante.sql
@@ -1,51 +1,62 @@
-INSERT INTO cargos (nome_titulo, salario, horas_trabalho) VALUES
-('Gerente', 5000.00, 40),
-('Cozinheiro', 3000.00, 40),
-('Garçom', 2500.00, 30),
-('Auxiliar de Cozinha', 2200.00, 40),
-('Bartender', 2700.00, 35);
-INSERT INTO empregados (primeiro_nome, sobrenome, nome_do_meio, cargo) VALUES
-('João', 'Silva', 'Carlos', 1),
-('Maria', 'Pereira', 'Luiza', 2),
-('Carlos', 'Oliveira', 'José', 3),
-('Ana', 'Souza', 'Clara', 4),
-('Pedro', 'Barbosa', 'Almeida', 5);
-INSERT INTO folha_de_ponto (funcionario_id, hora_inicio, hora_fim) VALUES
-(1, '2023-11-14 08:00', '2023-11-14 16:00'),
-(2, '2023-11-14 09:00', '2023-11-14 17:00'),
-(3, '2023-11-14 10:00', '2023-11-14 14:00');
+create table if not exists cargos(
+	titulo_id serial primary key,
+	nome_titulo varchar,
+	salario numeric,
+	horas_trabalho int -- carga horária normal para o cargo
+);
 
-INSERT INTO nivel_menu_1 (nome_menu_n1) VALUES
-('Entradas'),
-('Pratos Principais'),
-('Sobremesas'),
-('Bebidas');
+create table if not exists empregados(
+	funcionario_id serial primary key,
+	primeiro_nome varchar,
+	sobrenome varchar,
+	nome_do_meio varchar,
+	cargo int references cargos -- cargo do empregado
+);
 
-INSERT INTO refeicoes (nome_refeicao, custo_refeicao) VALUES
-('Feijoada', 25.00),
-('Pizza Margherita', 30.00),
-('Sorvete de Chocolate', 10.00),
-('Caipirinha', 15.00);
+create table if not exists folha_de_ponto(
+	funcionario_id int references empregados,
+	hora_inicio timestamp, -- início real do dia de trabalho
+	hora_fim timestamp -- fim real do dia de trabalho
+);
 
-INSERT INTO nivel_menu_2 (menu_n1, refeicao, nome_menu_n2) VALUES
-(1, 1, 'Feijoada Executiva'),
-(2, 2, 'Pizza Família');
+-- MENU
+create table if not exists nivel_menu_1(
+	menu_n1_id serial primary key,
+	nome_menu_n1 varchar unique);
 
-INSERT INTO descontos (valor_desconto, nome_desconto) VALUES
-(5.00, 'Desconto de Aniversário'),
-(10.00, 'Desconto ifood');
+create table if not exists refeicoes(
+	refeicao_id serial primary key,
+	nome_refeicao varchar unique,
+	custo_refeicao numeric not null);
 
-INSERT INTO pedidos (garcom, desconto, status) VALUES
-(1, 1, 'Pedido aberto'),
-(2, 2, 'Pedido fechado');
+create table if not exists nivel_menu_2(
+	menu_n2_id serial primary key,
+	menu_n1 int references nivel_menu_1 (menu_n1_id),
+	refeicao int references refeicoes (refeicao_id),
+	nome_menu_n2 varchar);
 
-INSERT INTO detalhes_pedidos (pedido_id, refeicao_id, quantidade) VALUES
-(1, 1, 2),
-(2, 2, 1);
+-- PEDIDOS
+create table if not exists descontos(
+	desconto_id serial primary key,
+	valor_desconto numeric,
+	nome_desconto varchar unique);
 
-INSERT INTO contas (pedido_id, valor_pago) VALUES
-(1, 50.00),
-(2, 30.00);
+create table if not exists pedidos(
+	pedido_id serial primary key,
+	garcom int references empregados (funcionario_id) not null,
+	data_pedido timestamp default current_timestamp,
+	desconto int references descontos (desconto_id) default 1,
+	status varchar default 'Pedido aberto');
 
+create table if not exists detalhes_pedidos(
+	pedido_id int references pedidos (pedido_id) on delete cascade on update cascade,
+	refeicao_id int references refeicoes (refeicao_id) not null,
+	quantidade int default 1
+	);
 
-
+create table if not exists contas(
+	conta_id serial primary key,
+	pedido_id int references pedidos (pedido_id) unique,
+	valor_pago numeric default 0,
+	data_pagamento timestamp default current_timestamp
+	);
